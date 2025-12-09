c29fe01f7fdeab335bfe24a8d3bc210767e28a7c
prasanth844 <135524731+prasanth844@users.noreply.github.com>
2025-12-09 16:42:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll(".code");
+codes[0].focus();
+codes.forEach((num ,index) => {
+
+	num.addEventListener("input",(e) => {
+		const value = e.target.value;
+		if(!/^[0-9]$/.test(value)){
+			e.target.value = "";
+			return;
+		}
+		if(index < codes.length - 1){
+			codes[index + 1].focus();
+		}
+	});
+
+	num.addEventListener("keydown" ,(e) => {
+		if(e.key === "Backspace"){
+			if(num.value === ""){
+				if(index > 0){
+					codes[index - 1].value = "";
+					codes[index - 1].focus();
+				}
+			}
+			else{
+				num.value = "";
+			}
+		}
+	});
+});
